4c49ea3ee73c349bde887ce19f9f7fd1a7c26155
Simon Katz <sk@nomistech.com>
2019-09-24 16:11:59 +0100

    Move dependency on tools.namespace from dev profile to top level

--- project.clj
+++ project.clj
@@ -3,8 +3,9 @@
   :url "https://github.com/simon-katz/nomis-clj-repl-tools"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  ;; TODO Upgrade dependencies
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/tools.namespace "0.3.0-alpha4"]]
   :profiles {:dev {:source-paths ["dev"]
-                   :dependencies [[org.clojure/tools.namespace "0.3.0-alpha4"] ; TODO Upgrade dependency
-                                  [midje "1.9.1"]]
+                   :dependencies [[midje "1.9.1"]]
                    :plugins [[lein-midje "3.2.1"]]}})
