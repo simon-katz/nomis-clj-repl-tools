67a04ace8c9a5f27bffa316b13d82d1f4f4490aa
Simon Katz <sk@nomistech.com>
2019-09-24 16:15:26 +0100

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -3,9 +3,8 @@
   :url "https://github.com/simon-katz/nomis-clj-repl-tools"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  ;; TODO Upgrade dependencies
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/tools.namespace "0.3.0-alpha4"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/tools.namespace "0.3.1"]]
   :profiles {:dev {:source-paths ["dev"]
-                   :dependencies [[midje "1.9.1"]]
+                   :dependencies [[midje "1.9.9"]]
                    :plugins [[lein-midje "3.2.1"]]}})
